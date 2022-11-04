34a400c16d358cfecc21c570afe6f73c932210c7
Aayush99910 <amaharjann01@gmail.com>
2022-11-04 12:51:50 +0545

    added handleEqution function and also added comments

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+// grabbing the user input element a element which will show the equation
 const input = document.querySelector("#input");
-let del = false;
+const finalOutput = document.querySelector("#final-output");
+let del = false; // del variable
 
+
+// This function takes in a string equation as parameter and 
+// it seperates two number from the equation and does the desired
+// operation. For example it takes in "9 + 1". It separates and assigns
+// firstNumber to 9 and secondNumber to 1. At last it checks which 
+// operation to perform.It converts the string into number 
+// and returns the end product. Here that is 10.
 function operation (equation, operator) {
     let firstNumber = '';
     let secondNumber = '';
@@ -25,56 +34,73 @@ function operation (equation, operator) {
     }
 }
 
+// This function first removes the "=" sign from the equation and passes
+// the arguments to needed function and at last it returns the end product. 
+function runOperation(equation) {
+    let newEquation;
 
-function runOperation() {
-    const finalOutput = document.querySelector("#final-output");
-    let equation = input.innerText;
-    finalOutput.textContent = equation;
-    let lastItem = equation.length - 1;
-    let newEquation = equation.slice(0, lastItem);
+    if (equation.includes("=")) {
+        let lastItem = equation.length - 1;
+        newEquation = equation.slice(0, lastItem);
+    }else {
+        newEquation = equation;
+    }
 
     if (newEquation.includes("+")) {
-        add(newEquation);
+        return add(newEquation);
     }else if (newEquation.includes("-")) {
-        subtract(newEquation);
+        return subtract(newEquation);
     }else if (newEquation.includes("*")) {
-        multiply(newEquation);
+        return multiply(newEquation);
     }else if (newEquation.includes("/")) {
-        divide(newEquation);
+        return divide(newEquation);
     }
 }
 
+
+// adds two numbers
+// this function takes in a equation and calls the operation function.
+// At last it returns the end product.
 function add (equation) {
-    const result = operation(equation, '+');
-    input.innerText = result;
+    return operation(equation, '+');
 }
 
 
+// subtracts two numbers
+// this function takes in a equation and calls the operation function.
+// At last it returns the end product.
 function subtract (equation) {
-    const result = operation(equation, "-");
-    input.innerText = result;
+    return operation(equation, "-");
 }
 
 
+// multiplies two numbers
+// this function takes in a equation and calls the operation function.
+// At last it returns the end product.
 function multiply (equation) {
-    const result = operation(equation, "*");
-    input.innerText = result;
+    return operation(equation, "*");
 }
 
 
+// divides two numbers
+// this function takes in a equation and calls the operation function.
+// At last it returns the end product.
 function divide (equation) {
-    const result = operation(equation, "/");
-    input.innerText = result;
+    return operation(equation, "/");
 }
 
+
+// this function renders the item when user presses on one i.e a button.
 function render(item) {
     if (input.innerText) {
-        input.innerText += item;
+        input.innerText += " " + item;
     }else {
         input.textContent = item;
     }
 } 
 
+
+// deletes a item from the string when user clicks the delete Button.
 function removeItem() {
     let equation = input.innerText;
     let lastIndex = equation.length - 1;
@@ -84,18 +110,55 @@ function removeItem() {
     del = false;
 }
 
+// I took this function from Ujwal Pansari aka God D. Ussop so for this function 
+// credit goes to him || HIS GITHUB PROFILE https://github.com/Captain-Usopp
+function handleEquation(equation) {
+
+	equation = equation.split(" ");
+	const operators = ['/', '*', '%', '+', '-'];
+	let firstNumber;
+	let secondNumber;
+	let operator;
+	let operatorIndex;
+	let result;
+
+	/*  
+		1. Perform calculations as per BODMAS Method
+		2. For that use operators array
+		3. after calculation of 1st numbers replace them with result
+		4. use splice method
+
+	*/
+	for (var i = 0; i < operators.length; i++) {
+		while (equation.includes(operators[i])) {
+			operatorIndex = equation.findIndex(item => item === operators[i]);
+			firstNumber = equation[operatorIndex-1];
+			operator = equation[operatorIndex];
+			secondNumber = equation[operatorIndex+1];
+            let equationString = `${firstNumber}${operator}${secondNumber}`;
+			result = runOperation(equationString);
+			equation.splice(operatorIndex - 1, 3, result);
+		}
+	}
+
+	return result;
+}
+
+// clears the content and everthing goes to blank
 const clearContent = () => {
-    const finalOutput = document.querySelector("#final-output");
     finalOutput.innerText = " ";
     input.innerText = " ";
 }
 
 
+// event listener for the buttons in the calculator
+// whenever a button is clicked this even fires up and the item
+// is rendered
 const btns = document.querySelectorAll("button");
 const btnsArray = Array.from(btns);
 btnsArray.forEach((button) => {
     button.addEventListener("click", (e) => {
-        if (e.target.innerText.toLowerCase() === "clear" || e.target.innerText.toLowerCase() === "delete") {
+        if (e.target.innerText.toLowerCase() === "ac" || e.target.innerText.toLowerCase() === "delete") {
             return // skips if the button are clear and delete
         } else {
             render(e.target.innerText);
@@ -104,12 +167,24 @@ btnsArray.forEach((button) => {
 });
 
 
-const equalBtn = document.querySelector("#equal");
-equalBtn.addEventListener("click", runOperation);
 
+// event listener for equal button
+// when this button is clicked it calls the handleEquation function
+// and at the end displays the end product to the user
+const equalBtn = document.querySelector("#equal");
+equalBtn.addEventListener("click", () => {
+    const initialText = input.innerText;
+    finalOutput.textContent = initialText;
+    input.innerText = handleEquation(input.innerText);
+});
+
+
+// event listener for clear button
 const clearBtn = document.querySelector("#clear");
 clearBtn.addEventListener("click", clearContent);
 
+
+// event listener for delete button
 const deleteBtn = document.querySelector("#delete");
 deleteBtn.addEventListener("click", () => {
     del = true;
